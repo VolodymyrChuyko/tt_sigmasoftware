0986685db71850b9d40b141526a3a379fd1ca36d
Volodymyr Chyuko <vol.chuyko@gmail.com>
2022-11-14 16:58:25 +0200

    Add programming  environment

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    '@mate-academy/eslint-config',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint'],
+  rules: {
+    indent: ['error', 2],
+    'linebreak-style': ['error', 'unix'],
+    quotes: ['error', 'single'],
+    semi: ['error', 'always'],
+  },
+};
---
+++ README.md
@@ -0,0 +1,11 @@
+# Test Task Solution
+
+typescript
+nodemon
+ts-node
+eslint
+types/node
+
+prettier?
+husky
+lint-staged
